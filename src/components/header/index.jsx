5edc9ba0d235cde56e34f296509ae76f7a2b5502
ghrabla <kamalghrabla@gmail.com>
2025-06-11 10:21:22 +0100

    wip

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,8 +1,11 @@
 const Header = () => {
   return (
-    <header id="header" className="mb-10">
+    <header
+      id="header"
+      className="mb-10 sm:text-center md:text-center lg:text-left"
+    >
       <div className="mb-2">
-        <div className="flex items-center">
+        <div className="flex items-center justify-center sm:justify-center md:justify-center lg:justify-start">
           <h1 className="inline-block text-2xl sm:text-3xl font-extrabold text-slate-900 tracking-tight dark:text-slate-800">
             Choose Your Skip Size
           </h1>
